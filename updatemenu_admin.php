b082bbcc5c0f64d9d11cb0b95e9d73e02bb62319
21081010027DiazPrisheila <21081010027@student.upnjatim.ac.id>
2023-05-13 14:54:18 +0700

    dd

--- updatemenu_admin.php
+++ updatemenu_admin.php
@@ -20,8 +20,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $id_menu = $_POST['id_menu'];
   $gambar = $_FILES['gambar']['name'];
+  $source = $_FILES['gambar']['tmp_name'];
   $nama_menu = $_POST['nama_menu'];
   $harga = $_POST['harga'];
+  $folder = './images/';
+
+  // Pindahkan file gambar yang diupload ke folder yang ditentukan
+  move_uploaded_file($source, $folder . $gambar);
     
   // Query SQL
   $sql = "UPDATE menu SET gambar='$gambar', nama_menu='$nama_menu', harga='$harga' WHERE id_menu='$id_menu'";
@@ -56,7 +61,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   </center>
 
   <main role="main">
-    <form action="updatemenu_admin.php" method="POST">
+    <form action="updatemenu_admin.php" method="POST" enctype="multipart/form-data">
       <?php while ($data = mysqli_fetch_array($result)) : ?>
 
         <div>
@@ -65,7 +70,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         </div>
         <div>
           <label>GAMBAR</label>
-          <input type="file" name="gambar" value="<?php echo $data['gambar']; ?>" required="required">
+          <input type="hidden" name="img" value="<?php echo $gambar ?>" >
+          <input type="file" name="gambar">
         </div>
         <div>
           <label>NAMA MENU</label>
@@ -76,7 +82,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
           <input type="text" placeholder="harga" name="harga" value="<?php echo $data['harga']; ?>" required="required">
         </div>
       <?php endwhile; ?>
-      <button type="submit" value="upload">Simpan</button>
+      <button type="submit" name="kirim" value="update">Simpan</button>
     </form>
   </main>
 </body>
