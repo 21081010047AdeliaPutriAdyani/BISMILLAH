0331aa87f34a8e76c96b447c7b5ed6e3092dbf36
21081010215ShintaDwiAnggraeni <21081010215@student.upnjatim.ac.id>
2023-05-13 11:32:18 +0700

    shin

---
+++ pembukuan.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Laporan Keuangan</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Tautkan file CSS Bootstrap -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container mt-5">
+		<div class="row">
+			<div class="col-md-12">
+				<h2 class="text-center mb-5">Laporan Penjualan</h2>
+				<table class="table table-bordered">
+					<thead>
+						<tr bgcolor="silver">
+							<th>Tanggal/Jam</th>
+							<th>Total Harga</th>
+						</tr>
+					</thead>
+                    <tbody>
+                    <div class="row">
+					<div class="col-md-7 text-right">
+						<p>Total Perbulan: </p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Tautkan file JavaScript Bootstrap -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+                
+                <?php 
+                  $query = "SELECT * FROM selforder";
+                  $result = mysqli_query(connection(),$query);
+                 ?>
+
+                 <?php while($data = mysqli_fetch_array($result)): ?>
+                  <tr>
+                    <td><?php echo $data['Tanggal/Jam'];  ?></td>
+                    <td><?php echo $data['Total Harga'];  ?></td>
+                 </tr>
+                 <?php endwhile ?>
+                 <tbody>
+				</table>
+</body>
+</html>
