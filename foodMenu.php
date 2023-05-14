6afd3156c56ef928fe966c04bd3fd7e7b49d5888
ariffmhdd <ariffmhdd>
2023-05-14 14:44:41 +0700

    nw

---
+++ foodMenu.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="icon" href="images/Assistant Cashier and Bookkeeper.png">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
+    <title>Food Menu - Assier</title>
+    <style>
+        @media(min-width: 600px) {
+            .mx-auto {
+                width: 50%;
+            }
+        }
+    </style>
+</head>
+
+<body>
+    <br>
+    <div class="mx-auto">
+        <div class="row justify-content-center">
+            <div class="col-11">
+                <h5 class="text-center"></h5>
+                <div class="card">
+                    <div class="card-header">
+                        Silahkan pilih menu anda
+                    </div>
+                    <div class="card-body">
+                        <table class="table table-striped table-hover table-bordered">
+                            <tr>
+                                <th>Gambar menu</th>
+                                <th>Menu</th>
+                                <th>Harga Satuan</th>
+                                <th>Kuantitas</th>
+                            </tr>
+                        </table>
+                        <a href="#" class="btn btn-primary">Lanjutkan Pesanan</a>
+                    </div>
+                    <div class="card-footer text-body-secondary">
+                        <p>Jl.Raya Rungkut Madya, Gunung Anyar, Surabaya</p>
+                        <p>&copy; Copyright <strong>Assier</strong>. All Rights Reserved</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    </div>
+</body>
+
+</html>
