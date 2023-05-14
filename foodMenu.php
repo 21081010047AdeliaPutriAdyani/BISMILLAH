d68a956c991db0368774aca1deebbcb8b8f01b0a
21081010027DiazPrisheila <21081010027@student.upnjatim.ac.id>
2023-05-14 17:27:46 +0700

    h

--- foodMenu.php
+++ foodMenu.php
@@ -85,7 +85,7 @@ if (!$koneksi) {
                             </tbody>
                         </table>
                         <div class="col" align="right">
-                            <a href="foodMenu.php" class="btn btn-primary">Lanjutkan Pesanan</a>
+                            <a href="3" class="btn btn-primary">Lanjutkan Pesanan</a>
 
                         </div>
                     </div>
