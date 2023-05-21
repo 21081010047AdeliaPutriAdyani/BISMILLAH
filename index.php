1687b5463421c81a1715d3186fcbcf1156761e9b
ariffmhdd <ariffmhdd>
2023-05-21 18:54:37 +0700

    ssave

--- index.php
+++ index.php
@@ -17,8 +17,8 @@ if (isset($_POST['simpan'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-        crossorigin="anonymous"></script>
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+        </script>
     <title>Home - Assier</title>
     <style>
         @media(min-width: 600px) {
@@ -55,8 +55,8 @@ if (isset($_POST['simpan'])) {
                                     </div>
                                 </div>
 
-                                <a href="foodMenu.php"><input type="submit" class="btn btn-primary" name="simpan"
-                                        value="Mulai Pesanan"></a><br>
+                                <input type="submit" class="btn btn-secondary" name="simpan" value="Simpan">
+                                <a href=" foodMenu.php" class="btn btn-primary">Lanjutkan Pesanan</a>
                             </form>
                         </div>
                         <br>
